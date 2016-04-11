0037f3dccb5523fd837efcd21b8e16034cb2e529
Fantinel Lucas <lucas.fantinel@gmail.com>
2016-04-11 22:20:41 +0200

    jsais plus ce qui a été modifié mais je commit

--- src/main/java/fr/univ/tlse2/sfr/communication/EnregistreurKryo.java
+++ src/main/java/fr/univ/tlse2/sfr/communication/EnregistreurKryo.java
@@ -7,11 +7,11 @@ import java.util.List;
 import com.esotericsoftware.kryo.Kryo;
 
 /**
- * Classe utilitaire permettant d'enregistrer au près de Kryo les classes à envoyer sur le réseau.
+ * Classe utilitaire permettant d'enregistrer au près de Kryo les classes à  envoyer sur le réseau.
  * fournit une méthode statique permettant d'enregistrer toutes les classes de message auprès d'une instance de Kryo.
  */
 public final class EnregistreurKryo {
-	/** La liste des instances Kryo auprès desquelles on à déjà enregistrées les classes de messages.*/
+	/** La liste des instances Kryo auprès desquelles on à  déjà  enregistrées les classes de messages.*/
 	private static List<Kryo> liste_des_kryo_deja_traites = new LinkedList<Kryo>();
 	
 	/** Classe statique -> Constructeur privé. */
